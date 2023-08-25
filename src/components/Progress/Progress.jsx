a86307b6c4948da5814df151b7fedf66fda612cb
Dominic van Almsick <dominicvana@gmail.com>
2023-08-25 10:18:47 +0100

    chore: prepare to pull from develop

--- src/components/Progress/Progress.jsx
+++ src/components/Progress/Progress.jsx
@@ -1,12 +1,13 @@
 import progressBar from '../../assets/progress-bar.svg';
+import './Progress.scss';
 
 export default function Progress({ value }) {
   return (
     <article className="progress">
-      <h2>Your progress</h2>
+      <h2 className="progress__heading">Your progress</h2>
       <span>£520</span>
       <img src={progressBar} alt="progress bar" />
-      <span>1%</span>
+      <span className="progress_percentage">1%</span>
       <p>Keep swimming forward, you&apos;re on Octo-track!</p>
     </article>
   );
