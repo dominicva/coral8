04a02b2d335712b3ad7a0a97de3b27f9492dbcbe
Dominic van Almsick <dominicvana@gmail.com>
2023-08-25 11:38:18 +0100

    feat: add initial energy usage

--- src/App.js
+++ src/App.js
@@ -3,23 +3,22 @@ import Page from './components/Page';
 import './App.scss';
 import NavBar from './components/NavBar/NavBar';
 import SelectedProductCard from './components/SelectedProductCard/SelectedProductCard';
+import { PieChart } from './components/PieChart';
+import { EnergyUsage } from './components/EnergyUsage';
+import { Progress } from './components/Progress';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route
-            path="/"
-            element={
-              <div>
-                <h1>Home</h1>
-              </div>
-            }
-          />
+          <Route path="/" element={<div className="home">Home</div>} />
           <Route path="/page" element={<Page />} />
         </Routes>
+        <PieChart />
         <SelectedProductCard />
+        <Progress />
+        <EnergyUsage />
         <NavBar />
       </BrowserRouter>
     </div>
