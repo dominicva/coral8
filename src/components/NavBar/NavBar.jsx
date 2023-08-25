9e3437f64defb8a0c06be4e31a1ba72e6bafb9ff
Dominic van Almsick <dominicvana@gmail.com>
2023-08-25 13:00:26 +0100

    chore: remove unused class name

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,7 +3,7 @@ import navBar from '../../assets/nav-bar.svg';
 export default function NavBar() {
   return (
     <nav className="nav">
-      <img className="nav__nav-bar" src={navBar} alt="Nav bar" />
+      <img src={navBar} alt="Nav bar" />
     </nav>
   );
 }
