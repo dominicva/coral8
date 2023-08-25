f5e13977732d66193fdf846dfacadc4523480cbe
Dominic van Almsick <dominicvana@gmail.com>
2023-08-25 10:07:45 +0100

    feat: add pie chart component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Page from './components/Page';
 import './App.scss';
 import NavBar from './components/NavBar/NavBar';
